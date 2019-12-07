049ce3df3dea368914c0a5946e5d0d901a230d7b
Javier Gómez <javiergomezve@gmail.com>
2019-12-07 12:58:39 -0500

    [add] heroku support

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({ app: 'React Bot '});
+    res.send({ app: 'React Bot' });
 });
 
-app.listen(5000, () => console.log('Runing on http://127.0.0.1:5000'));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Runing on http://127.0.0.1:${PORT}`));
