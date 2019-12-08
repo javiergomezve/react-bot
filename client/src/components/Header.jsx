9d88b1cb74c28479f86a0135c391caa71315466c
Javier Gómez <javiergomezve@gmail.com>
2019-12-08 12:54:34 -0500

    [add] materialize

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,14 +5,18 @@ const Header = () => {
 
     return (
         <nav>
-            <ul>
-                <li>
-                    <Link to={'/shop'}>Shop</Link>
-                </li>
-                <li>
-                    <Link to={'/about'}>About</Link>
-                </li>
-            </ul>
+            <div className="nav-wrapper">
+                <Link to={'/'} className="brand-logo">IT courses</Link>
+                
+                <ul id="nav-mobile" className="right hide-on-med-and-down">
+                    <li>
+                        <Link to={'/shop'}>Shop</Link>
+                    </li>
+                    <li>
+                        <Link to={'/about'}>About</Link>
+                    </li>
+                </ul>
+            </div>
         </nav>
     );
 };
